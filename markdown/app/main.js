02704da9996042ca6971804f3deab111b6f18897
dannash100 <danster15@gmail.com>
2018-10-14 16:50:51 +1300

    markdown functionality working

--- markdown/app/main.js
+++ markdown/app/main.js
@@ -3,8 +3,11 @@ const { app, BrowserWindow } = require('electron')
 let mainWindow = null
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow()
+    mainWindow = new BrowserWindow({show: false})
     mainWindow.loadFile('./app/index.html')
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
     mainWindow.on('closed', () => {
         mainWindow = null
     })
