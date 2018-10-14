ef18b2ca40e89b222296c2efb429646d8bae493a
dannash100 <danster15@gmail.com>
2018-10-14 21:14:14 +1300

    file filters

--- markdown/app/main.js
+++ markdown/app/main.js
@@ -1,14 +1,37 @@
-const { app, BrowserWindow } = require('electron')
+const {
+  app,
+  BrowserWindow,
+  dialog
+} = require('electron')
+
+const fs = require('fs')
 
 let mainWindow = null
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({show: false})
-    mainWindow.loadFile('./app/index.html')
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show()
-    })
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    })
+  mainWindow = new BrowserWindow({
+    show: false
+  })
+  mainWindow.loadFile('./app/index.html')
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+    getFileFromUser()
+  })
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 })
+
+const getFileFromUser = () => {
+  const files = dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [
+      {name: 'Text Files', extensions: ['txt']},
+      {name: 'Markdown Files', extensions: ['md', 'markdown']}
+    ]
+  })
+  if (!files) { return }
+  const file = files[0]
+  const content = fs.readFileSync(file).toString()
+  console.log(content)
+}
