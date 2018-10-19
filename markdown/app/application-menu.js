7b70dd45e76aa1d113fb1d03cf14382f794eb6eb
dannash100 <danster15@gmail.com>
2018-10-19 14:19:49 +1300

    menu options

--- markdown/app/application-menu.js
+++ markdown/app/application-menu.js
@@ -1,25 +1,97 @@
-const {app, BroswerWindow, Menu, shell } = require('electron')
+const { app, BroswerWindow, Menu, shell } = require('electron')
 const mainProcess = require('./main')
 
 const template = [{
-  label: 'Edit',
-  submenu: [
-    {
-      label: 'Copy',
-      accelerator: 'CommandOrControl+C',
-      role: 'copy'
-    },
-    {
-      label: 'Paste',
-      accelerator: 'CommandOrControl+V',
-      role: 'paste'
-    }
-  ]
-}]
+    label: 'Edit',
+    submenu: [{
+        label: 'Undo',
+        accelerator: 'CommandOrControl+Z',
+        role: 'undo'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CommandOrControl+Z',
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        type: 'Cut',
+        accelerator: 'CommandOrControl+X',
+        role: 'cut'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CommandOrControl+C',
+        role: 'copy'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CommandOrControl+V',
+        role: 'paste'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CommandOrControl+A',
+        role: 'selectall'
+      }
+    ]
+  },
+  {
+    label: 'Window',
+    submenu: [{
+        label: 'Minimize',
+        accelerator: 'CommandOrControl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Close',
+        accelerator: 'CommandOrControl+W',
+        role: 'close'
+      }
+    ]
+  }
+]
 
 if (process.platform === 'darwin') {
   const name = 'Markdown Editor'
-  template.unshift({ label: name }) // moves new item to beginning of template array for macOS
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: `About ${name}`,
+        role: 'about'
+      },
+      { type: 'seperator' },
+      {
+        label: 'Services',
+        role: 'services',
+        submenu: []
+      },
+      { type: 'seperator' },
+      {
+        label: `Hide ${name}`,
+        accelerator: 'Command+H',
+        role: 'hide'
+      },
+      {
+        label: 'Hide Others',
+        accelerator: 'Command+Alt+H',
+        role: 'hideothers'
+      },
+      {
+        label: 'Show All',
+        role: 'unhide'
+      },
+      { type: 'seperator' },
+      {
+        label: `Quit ${name}`,
+        accelerator: 'Command+Q',
+        click() { app.quit() }
+      }
+    ]
+  }) 
 }
 
 module.exports = Menu.buildFromTemplate(template)
