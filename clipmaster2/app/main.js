377e030b0d6256adf295238558b0968e12f5f490
dannash100 <danster15@gmail.com>
2018-10-24 07:54:20 +1300

    eslint env variables and begun work in renderer

--- clipmaster2/app/main.js
+++ clipmaster2/app/main.js
@@ -2,6 +2,10 @@ const Menubar = require('menubar');
 
 const menubar = Menubar();
 
-menubar.on('ready', () => {
-  console.log('Application is ready');
+// menubar.on('ready', () => {
+//   console.log('Application is ready');
+// });
+
+menubar.on('after-create-window', () => {
+  menubar.window.loadURL(`file://${__dirname}/index.html`);
 });
