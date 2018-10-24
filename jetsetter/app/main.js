44425863d8ec8debc41a243397bbc62585a783b0
dannash100 <danster15@gmail.com>
2018-10-24 15:46:21 +1300

    fixed git drama

---
+++ jetsetter/app/main.js
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 300,
+    height: 600,
+    minWidth: 300,
+    minHeight: 300,
+    show: false
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.jade`);
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+});
