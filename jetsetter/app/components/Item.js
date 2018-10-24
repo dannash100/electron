15e4c4883804bae2ba8c0f49835e9d83b67febe0
dannash100 <danster15@gmail.com>
2018-10-24 16:12:32 +1300

    Item, Item components hooked up

---
+++ jetsetter/app/components/Item.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Item = (({ packed, id, value, onCheckOff }) => {
+  return (
+    <article className="item">
+      <label>
+        <input type="checkbox" checked={packed} onChange={onCheckOff} />
+          {value}
+      </label>
+    </article>
+  );
+});
+
+export default Item
