8a02ea8364ec01403d88331ca71d5c7421943c19
dannash100 <danster15@gmail.com>
2018-10-25 21:33:45 +1300

    more tests

--- clipmaster2/test/spec.js
+++ clipmaster2/test/spec.js
@@ -20,8 +20,48 @@ describe('clipmaster2', function () {
   });
 
   it('shows an initial window', async () => {
-    const count = await app.client.getWindowCount()
-    return assert.equal(count, 1)
+    const count = await app.client.getWindowCount();
+    return assert.equal(count, 1);
   });
 
+  it('has the correct title', async () => {
+    const title = await app.client.waitUntilWindowLoaded().getTitle();
+    return assert.equal(title, 'Clipmaster 9000');
+  })
+
+  it('does not have the developer tools open', async () => {
+    const devToolsAreOpen = await app.client
+      .waitUntilWindowLoaded()
+      .browserWindow.isDevToolsOpened();
+    return assert.equal(devToolsAreOpen, false);
+  });
+
+  it('has a button with the text "Copy from Clipboard"', async () => {
+    const buttonText = await app.client
+      .getText('#copy-from-clipboard');
+    return assert.equal(buttonText, 'Copy from Clipboard');
+  });
+
+  it('should not have clippings when it starts up', async () => {
+    await app.client.waitUntilWindowLoaded();
+    const clippings = await app.client.$$('.clippings-list-item'); // $ - querySelector, $$ - querySelectorAll
+    return assert.equal(clippings.length, 0);
+  })
+
+  it('should have one clipping when the "Copy from Clipboard" button has been pressed', async () => {
+    await app.client.waitUntilWindowLoaded();
+    await app.client.click('#copy-from-clipboard');
+    const clippings = await app.client.$$('.clippings-list-item');
+    return assert.equal(clippings.length, 1)
+  });
+
+  it('should successfully remove a clipping', async () => {
+    await app.client.waitUntilWindowLoaded();
+    await app.client
+      .click('#copy-from-clipboard')
+      .moveToObject('.clippings-list-item')
+      .click('.remove-clipping');
+    const clippings = await app.client.$$('.clippings-list-item');
+    return assert.equal(clippings.length, 0);
+  })
 });
